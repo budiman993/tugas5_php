228bed566f217228c1349583dd82411755dad1ff
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-10-08 10:30:49 +0700

    Add files via upload

--- tugas5_1.php
+++ tugas5_1.php
@@ -24,28 +24,28 @@ $f = false;
 <td>false</td>
 <td>false</td>
 <td><?php echo"".printf($f && $f); ?></td>
-<td><?php echo"".printf($f or $f); ?></td>
+<td><?php echo"".printf($f || $f); ?></td>
 </tr>
 
 <tr>
 <td>false</td>
 <td>true</td>
 <td><?php echo"".printf($f && $t); ?></td>
-<td><?php printf($f or $t); ?></td>
+<td><?php printf($f || $t); ?></td>
 </tr>
 
 <tr>
 <td>true</td>
 <td>false</td>
 <td><?php echo"".printf($t && $f); ?></td>
-<td><?php printf($t or $f); ?></td>
+<td><?php printf($t || $f); ?></td>
 </tr>
 
 <tr>
 <td>true</td>
 <td>true</td>
 <td><?php printf($t && $t); ?></td>
-<td><?php printf($t or $t); ?></td>
+<td><?php printf($t || $t); ?></td>
 </tr>
 </table>
 
